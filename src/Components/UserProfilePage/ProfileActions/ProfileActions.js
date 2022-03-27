dbbfbea5dd7b1a005694c67fe67e655f45dd8a1d
dinamaged1 <76622624+dinamaged1@users.noreply.github.com>
2022-03-28 01:28:55 +0200

    Added user info

--- src/Components/UserProfilePage/ProfileActions/ProfileActions.js
+++ src/Components/UserProfilePage/ProfileActions/ProfileActions.js
@@ -22,13 +22,13 @@ function ProfileActions(props) {
   } else {
     actionsRendered = (
       <Fragment>
-        <div className={`${classes.settingButton} col-1`}>
+        <div className={`${classes.settingButton} col-2`}>
           <MoreHorizOutlinedIcon />
         </div>
-        <div className={`${classes.messageButton} col-1 d-flex `}>
+        <div className={`${classes.messageButton} col-2 d-flex `}>
           <MailOutlineRoundedIcon fontsize="meduim" />
         </div>
-        <div className={`col-2 pe-0 me-3`}>
+        <div className={`col-2 pe-0 ${!isFollowing? 'me-3': 'me-4'}`}>
           <FollowButton
             isFollowing={isFollowing}
             onFollow={onFollowHandeler}
