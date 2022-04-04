84f3a83caba5f511fa5d72d48ad83a92195bbae5
andrew9991 <andrew.ramses10@gmail.com>
2022-04-04 19:21:55 +0200

    font adjustment

--- src/Components/TimeLinePage/FeedTweet.js
+++ src/Components/TimeLinePage/FeedTweet.js
@@ -83,7 +83,7 @@ export default function FeedTweet(props) {
             />
           </div>
           &nbsp;
-          <p className={classes.gray + " " + classes.minip}>
+          <p className={classes.gray + " " + classes.minip + " " + classes.fs15}>
             @{props.userName}
           </p>
           <div className={classes.hoverProfile + " " + classes.bot}>
@@ -97,10 +97,10 @@ export default function FeedTweet(props) {
             />
           </div>
           &nbsp;<p className={classes.gray}>.</p>&nbsp;
-          <p className={classes.gray + " " + classes.underline}>{getDateDiff(props.date)}</p>{" "}
+          <p className={classes.gray + " " + classes.underline + " " + classes.fs15}>{getDateDiff(props.date)}</p>{" "}
           {/*placeholder */}
         </div>
-        <p>{props.text}</p>
+        <p className={classes.fs15}>{props.text}</p>
 
         {props.img && <img className={classes.tweetImg} src={props.img}></img>}
         <div className={classes.attributes}>
