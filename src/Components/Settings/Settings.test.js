be7d83fa5980ccc31b6febb27facc5431684bcf4
MillaniaSameh <samehmillania@gmail.com>
2022-04-04 18:50:45 +0200

    fixed settings test

--- src/Components/Settings/Settings.test.js
+++ src/Components/Settings/Settings.test.js
@@ -1,9 +1,16 @@
 import { render, screen } from "@testing-library/react";
 import Settings from "./Settings";
-describe("testing Settings component", () => {
-  test("the component renders successfully", () => {
-    // render(<Settings></Settings>);
-    // const element = screen.findByText("");
-    // expect(element).not.toBeNull();
-  });
+
+test("the component renders successfully", () => {
+  render(<Settings />);
+  const element = screen.findByText("");
+  expect(element).not.toBeNull();
 });
+
+// describe("testing Settings component", () => {
+//   test("the component renders successfully", () => {
+//     // render(<Settings></Settings>);
+//     // const element = screen.findByText("");
+//     // expect(element).not.toBeNull();
+//   });
+// });
