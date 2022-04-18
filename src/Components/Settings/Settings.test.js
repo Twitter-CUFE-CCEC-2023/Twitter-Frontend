ddfc1533ff609619a194f3b7c557a26c2a2d0562
MillaniaSameh <samehmillania@gmail.com>
2022-04-18 15:40:36 +0200

    devops file added (src->components->axios.js)

--- src/Components/Settings/Settings.test.js
+++ src/Components/Settings/Settings.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import Settings from "./Settings";
 
 test("the component renders successfully", () => {
-  render(<Router> <Settings /> </Router>);
+  render(
+    <BrowserRouter>
+      {" "}
+      <Settings />{" "}
+    </BrowserRouter>
+  );
   const element = screen.findByText("");
   expect(element).not.toBeNull();
 });
