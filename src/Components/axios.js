73fb60f8667876a0e13cb3ef2b1e82a207a39591
Sandra Hany <67375539+sandrahanyh@users.noreply.github.com>
2022-04-23 17:24:30 +0200

    Update axios.js

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://BACKENDLB-885286453.us-east-1.elb.amazonaws.com",
+  baseURL: "http://backendlb-1541065125.us-east-1.elb.amazonaws.com/",
 });
 
 export default instance;
