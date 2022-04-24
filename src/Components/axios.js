825ca9b37b9d5061933bbbd9522d011a0a7bda60
andrew9991 <andrew.ramses10@gmail.com>
2022-04-24 17:32:10 +0200

    Update axios.js

--- src/Components/axios.js
+++ src/Components/axios.js
@@ -6,7 +6,7 @@ const instance = axios.create({
       'Access-Control-Allow-Credentials':true,
       'Access-Control-Allow-Origin': '*',
       'Content-Type': 'application/json; charset=utf-8',
-      'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MjRhNGE5NGM2NjczOGYxMzg1NGI0NzQiLCJ1c2VybmFtZSI6ImFtcnpha2kiLCJpYXQiOjE2NTA3OTg3MDR9.rubqGuQ7HdZZNnWxValrJWdHVGbxtXv1fY5N9dXhneI',
+      'Authorization': `Bearer ${localStorage.getItem('token')}` ,
     }
 });
 
