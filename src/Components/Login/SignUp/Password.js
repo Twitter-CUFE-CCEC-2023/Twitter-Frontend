dc2d32da90c95845e5bab84e06c4be00453d0ae3
Seif Emam <sagetoshiro@hotmail.com>
2022-05-15 20:19:07 +0200

    stupid me

--- src/Components/Login/SignUp/Password.js
+++ src/Components/Login/SignUp/Password.js
@@ -9,44 +9,44 @@ import Alert from "./Alert/Alert"
 import axios from "../../axios";
 
 const Mail = (props) => {
-    useEffect(() => {
-        const Name = JSON.parse(localStorage.getItem("Name"));
-        const PW = JSON.parse(localStorage.getItem("Password"));
-        const Email = JSON.parse(localStorage.getItem("Email"));
-        const phone = JSON.parse(localStorage.getItem("PhoneNumber"));
-        const Username = JSON.parse(localStorage.getItem("Username"));
+    // useEffect(() => {
+    //     const Name = JSON.parse(localStorage.getItem("Name"));
+    //     const PW = JSON.parse(localStorage.getItem("Password"));
+    //     const Email = JSON.parse(localStorage.getItem("Email"));
+    //     const phone = JSON.parse(localStorage.getItem("PhoneNumber"));
+    //     const Username = JSON.parse(localStorage.getItem("Username"));
 
-        let userObject = {
-            email: Email,
-            username: Username,
-            password: PW,
-            name: Name,
-            gender: props.gender,
-            birth_date: props.birth,
-        };
+    //     let userObject = {
+    //         email: Email,
+    //         username: Username,
+    //         password: PW,
+    //         name: Name,
+    //         gender: props.gender,
+    //         birth_date: props.birth,
+    //     };
 
-        console.log(userObject);
+    //     console.log(userObject);
 
-        if (phone.length !== 0) {
-            userObject["phone_number"] = phone;
-        }
+    //     if (phone.length !== 0) {
+    //         userObject["phone_number"] = phone;
+    //     }
 
-        axios
-            .post("/auth/signup", userObject, {
-                headers: { "Content-Type": "application/json" },
-            })
-            .then((response) => {
-                console.log(response);
-                if (response.status === 200) {
-                    localStorage.setItem("UserInfo", JSON.stringify(response.data.user));
-                    localStorage.removeItem("Name");
-                    localStorage.removeItem("Password");
-                    localStorage.removeItem("PhoneNumber");
-                    localStorage.removeItem("Username");
-                }
-            })
-            .catch((err) => { });
-    }, []);
+    //     axios
+    //         .post("/auth/signup", userObject, {
+    //             headers: { "Content-Type": "application/json" },
+    //         })
+    //         .then((response) => {
+    //             console.log(response);
+    //             if (response.status === 200) {
+    //                 localStorage.setItem("UserInfo", JSON.stringify(response.data.user));
+    //                 localStorage.removeItem("Name");
+    //                 localStorage.removeItem("Password");
+    //                 localStorage.removeItem("PhoneNumber");
+    //                 localStorage.removeItem("Username");
+    //             }
+    //         })
+    //         .catch((err) => { });
+    // }, []);
     const [alert, setAlert] = useState(true);
 
     const handleClick = (val) => {
