98d7009bc63bf1af27ca40900b85b34f9c2ee202
MinaLMG <beandro6@gmail.com>
2022-05-20 14:30:53 +0200

    added Modal component to show under construction page

--- src/Components/Settings/SettingsSection/SettingsSectionChoice.js
+++ src/Components/Settings/SettingsSection/SettingsSectionChoice.js
@@ -1,7 +1,12 @@
 import classes from "./SettingsSectionChoice.module.css";
 function SettingsSectionChoice(props) {
   return (
-    <div className={`${classes.choice} ${props.chosen ? classes.chosen : ""}`}>
+    <div
+      className={`${classes.choice} ${props.chosen ? classes.chosen : ""}`}
+      onClick={
+        props.name.toLowerCase().trim() != "your account" ? props.onShow : ""
+      }
+    >
       {props.name}
       <svg
         viewBox="0 0 24 24"
--- src/Components/UI/Modal.js
+++ src/Components/UI/Modal.js
@@ -1,18 +1,33 @@
 import React from "react";
 import classes from "./Modal.module.css";
+import ReactDOM from "react-dom";
+const Background = (props) => {
+  return <div className={classes.background} onClick={props.onHide}></div>;
+};
+const Body = (props) => {
+  return (
+    <div className={classes.body}>
+      <div className={classes.header}>{props.data.header}</div>
+      <div className={classes.message}>{props.data.message}</div>
+      <div className={classes.actions}>
+        <button className={classes.button} onClick={props.onHide}>
+          {props.data.button}
+        </button>
+      </div>
+    </div>
+  );
+};
 function Modal(props) {
   return (
     <React.Fragment>
-      <div className={classes.background} onClick={props.onHide}></div>
-      <div className={classes.body}>
-        <div className={classes.header}>{props.data.header}</div>
-        <div className={classes.message}>{props.data.message}</div>
-        <div className={classes.actions}>
-          <button className={classes.button} onClick={props.onHide}>
-            {props.data.button}
-          </button>
-        </div>
-      </div>
+      {ReactDOM.createPortal(
+        <Background onHide={props.onHide}></Background>,
+        document.getElementById("background")
+      )}
+      {ReactDOM.createPortal(
+        <Body onHide={props.onHide} data={props.data}></Body>,
+        document.getElementById("background")
+      )}
     </React.Fragment>
   );
 }
