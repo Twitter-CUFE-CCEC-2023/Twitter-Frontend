b1d35b7baa1928cb9333a4066352f081477d2daf
YoussefMokhtarr <youssef.mokhtar.ym@gmail.com>
2022-05-25 01:48:31 +0200

    Function documents for the notifications and search

--- src/Components/Notifications/Mentions/SingleMentionNotification.js
+++ src/Components/Notifications/Mentions/SingleMentionNotification.js
@@ -5,6 +5,14 @@ import classes from "./SingleMentionNotification.module.css";
 import instance from "../../axios";
 
 function SingleMentionNotification(props) {
+  /**
+   * @module SingleMentionNotification
+   */
+  /**
+   * @name ReadNote
+   * @function ReadNote
+   * @description This function has the PUT request that is responsible for changing the 'is_read' property of each reply notification from 'false' to 'true'
+   */
   const readNote = async () => {
     await instance.put("/read-notification", { notificationId: props.note_id });
   };
