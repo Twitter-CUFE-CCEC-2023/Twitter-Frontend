6139caf2554ac208a10512844a79db5e3a3139c8
MillaniaSameh <samehmillania@gmail.com>
2022-05-28 00:09:24 +0200

    Edits

--- src/Components/Login/Buttons/ButtonSignUp.js
+++ src/Components/Login/Buttons/ButtonSignUp.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./ButtonSignUp.module.css";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
+import ErrorModal from "../FirstPage/BanModal/ErrorModal";
 
 const ButtonSignUp = (props) => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+
+  const handleOpenModal = (val) => {
+    setOpen(val);
+  };
+
   return (
     <div className={classes.container}>
-
-      <NavLink to="/Mail" className={classes.navLink}>
-        <div className={classes.buttonSignUp}>
-
-          <p className={classes.content}>Sign up with a phone number or email</p>
-        </div>
-      </NavLink>
+      {/* <NavLink to="/Mail" className={classes.navLink}> */}
+      <div className={classes.buttonSignUp} onClick={handleClick}>
+        <p className={classes.content}>Sign up with a phone number or email</p>
+      </div>
+      {/* </NavLink> */}
+      {open && <ErrorModal open={open} setOpenModalValue={handleOpenModal} />}
     </div>
   );
 };
